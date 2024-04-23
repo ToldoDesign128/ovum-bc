cb4a92849d40c9753f80101535d41fb8b2e1a2e4
Federico <fede.toldo96@gmail.com>
2024-04-23 14:57:51 +0200

    page servizi and change ped-block

--- page-servizi.php
+++ page-servizi.php
@@ -3,6 +3,137 @@
 get_header();
 ?>
 <main>
+    <!-- Hero -->
     <?php get_template_part("template-parts/hero/hero-servizi"); ?>
+
+    <!-- Sezione chiropratica -->
+    <section class="tradizionale container">
+        <h2 class="tradizionale__title"><?php echo esc_html(get_field('titolo_tradizionale_servizi')); ?></h2>
+        <div class="tradizionale__content">
+            <h3 class="tradizionale__content__title"><?php echo esc_html(get_field('sottotitolo_tradizionale_servizi')); ?></h3>
+            <div class="tradizionale__content__box">
+                <div class="tradizionale__content__box__text margin-0 wysiwyg"><?php the_field('testo_tradizionale_servizi'); ?></div>
+                <?php
+                if (have_rows('elenco_avanzata_servizi')) : ?>
+                    <ul class="tradizionale__content__box__list">
+
+                        <?php
+                        while (have_rows('elenco_avanzata_servizi')) : the_row();
+
+                            // Load sub field value.
+                            $testo_ul_1 = get_sub_field('testo_elenco'); ?>
+
+                            <li>
+                                <img src="<?php echo get_template_directory_uri() . '/assets/img/acf/icona_check.svg'; ?>" alt="">
+                                <?php echo esc_html($testo_ul_1); ?>
+                            </li>
+
+                        <?php
+                        endwhile; ?>
+                    </ul>
+                <?php
+                endif; ?>
+                <div class="tradizionale__content__box__text wysiwyg"><?php the_field('testo_2_tradizionale_servizi'); ?></div>
+            </div>
+            <?php
+            $image_trad = get_field('immagine_tradizionale_servizi');
+            if (!empty($image_trad)) : ?>
+                <div class="tradizionale__content__image">
+                    <img src="<?php echo esc_url($image_trad['url']); ?>" alt="<?php echo esc_attr($image_trad['alt']); ?>" />
+                </div>
+            <?php endif; ?>
+        </div>
+    </section>
+
+    <!-- Sezione avanzata -->
+    <section class="avanzata container">
+        <h2 class="avanzata__title"><?php echo esc_html(get_field('titolo_avanzata_servizi')); ?></h2>
+        <div class="avanzata__content">
+            <h3 class="avanzata__content__title"><?php echo esc_html(get_field('sottotitolo_avanzata_servizi')); ?></h3>
+            <div class="avanzata__content__box">
+                <div class="avanzata__content__box__text margin-0 wysiwyg"><?php the_field('testo_avanzata_servizi'); ?></div>
+                <?php
+                if (have_rows('elenco_avanzata_servizi')) : ?>
+                    <ul class="avanzata__content__box__list">
+
+                        <?php
+                        while (have_rows('elenco_avanzata_servizi')) : the_row();
+
+                            // Load sub field value.
+                            $testo_ul_2 = get_sub_field('testo_elenco'); ?>
+
+                            <li>
+                                <img src="<?php echo get_template_directory_uri() . '/assets/img/acf/icona_check.svg'; ?>" alt="">
+                                <?php echo esc_html($testo_ul_2); ?>
+                            </li>
+
+                        <?php
+                        endwhile; ?>
+                    </ul>
+                <?php
+                endif; ?>
+                <div class="avanzata__content__box__text wysiwyg"><?php the_field('testo_2_avanzata_servizi'); ?></div>
+            </div>
+            <?php
+            $image_avanzata = get_field('immagine_avanzata_servizi');
+            if (!empty($image_avanzata)) : ?>
+                <div class="avanzata__content__image">
+                    <img  src="<?php echo esc_url($image_avanzata['url']); ?>" alt="<?php echo esc_attr($image_avanzata['alt']); ?>" />
+                </div>
+            <?php endif; ?>
+            <h3 class="avanzata__content__title margin-75"><?php echo esc_html(get_field('sottotitolo_avanzata_servizi_2')); ?></h3>
+            <div class="avanzata__content__box margin-75">
+                <div class="avanzata__content__box__text margin-0 wysiwyg"><?php the_field('testo_avanzata_servizi_3'); ?></div>
+                <?php
+                if (have_rows('elenco_avanzata_servizi_2')) : ?>
+                    <ul class="avanzata__content__box__list">
+
+                        <?php
+                        while (have_rows('elenco_avanzata_servizi_2')) : the_row();
+
+                            // Load sub field value.
+                            $testo_ul_3 = get_sub_field('testo_elenco'); ?>
+
+                            <li>
+                                <img src="<?php echo get_template_directory_uri() . '/assets/img/acf/icona_check.svg'; ?>" alt="">
+                                <?php echo esc_html($testo_ul_3); ?>
+                            </li>
+
+                        <?php
+                        endwhile; ?>
+                    </ul>
+                <?php
+                endif; ?>
+            </div>
+            <h3 class="avanzata__content__title margin-75"><?php echo esc_html(get_field('sottotitolo_avanzata_servizi_3')); ?></h3>
+            <div class="avanzata__content__box margin-75">
+                <div class="avanzata__content__box__text wysiwyg"><?php the_field('testo_3_avanzata_servizi'); ?></div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Prima e dopo -->
+    <div class="container">
+        <h3 class="titolo-ped-home"><?php echo esc_html(get_field('titolo_ped_servizi')); ?></h3>
+    </div>
+    <?php get_template_part("template-parts/prima-e-dopo"); ?>
+
+    <!-- Post Block -->
+    <div class="container">
+        <h3 class="titolo-ped-home"><?php echo esc_html(get_field('titolo_blog_servizi')); ?></h3>
+        <h2 class="subtitolo-post-home"><?php echo esc_html(get_field('sottotitolo_blog_servizi')); ?></h2>
+    </div>
+    <?php get_template_part("template-parts/post-block"); ?>
+
+    <!-- Video Block -->
+    <div class="container">
+        <h3 class="titolo-ped-home"><?php echo esc_html(get_field('titolo_video_servizi')); ?></h3>
+    </div>
+    <?php get_template_part("template-parts/video-block"); ?>
+
+    <!-- CTA -->
+    <?php get_template_part("template-parts/cta"); ?>
+
+
 </main>
 <?php get_footer(); ?>
--- template-parts/prima-e-dopo.php
+++ template-parts/prima-e-dopo.php
@@ -1,38 +1,44 @@
 <!-- Prima e Dopo -->
 <section class="container ped">
     <div class="ped__content">
-        <p class="ped__content__title">I benefici della chiropratica.</p>
-        <p class="ped__content__text">Ogni colonna ha la propria storia. In base alle sue condizioni e ai cambiamenti che subisce, può dare origine a disturbi di varia entità, come dolori e immobilità.</p>
-        <p class="ped__content__text">La sua manipolazione, da parte di mani esperte, aiuta il sistema nervoso a recuperare la propria efficienza energetica, favorendo la riattivazione dei processi di autoguarigione, di cui il nostro organismo è naturalmente dotato.</p>
-        <p class="ped__content__text">Queste sono le storie che il nostro centro chiropratico vuole raccontare.</p>
-        <!-- Stiky Post CTA -->
-        <a href="<?php the_permalink(); ?>" class="button-gray mt-3">
-            <span class="text">Prenota una visita
-                <svg xmlns="http://www.w3.org/2000/svg" width="11.024" height="11.024" viewBox="0 0 11.024 11.024" class="ml-1">
-                    <g id="Icon_feather-arrow-up-right" data-name="Icon feather-arrow-up-right" transform="translate(0.424 0.6)">
-                        <path id="Tracciato_26" data-name="Tracciato 26" d="M10.5,20.5l10-10" transform="translate(-10.5 -10.5)" fill="none" stroke="#000" stroke-linejoin="round" stroke-width="1.2" />
-                        <path id="Tracciato_27" data-name="Tracciato 27" d="M10.5,10.5h10v10" transform="translate(-10.5 -10.5)" fill="none" stroke="#000" stroke-linejoin="bevel" stroke-width="1.2" />
-                    </g>
-                </svg>
-            </span>
-            <span class="text-1">Prenota una visita
-                <svg xmlns="http://www.w3.org/2000/svg" width="11.024" height="11.024" viewBox="0 0 11.024 11.024" class="ml-1">
-                    <g id="Icon_feather-arrow-up-right" data-name="Icon feather-arrow-up-right" transform="translate(0.424 0.6)">
-                        <path id="Tracciato_26" data-name="Tracciato 26" d="M10.5,20.5l10-10" transform="translate(-10.5 -10.5)" fill="none" stroke="#000" stroke-linejoin="round" stroke-width="1.2" />
-                        <path id="Tracciato_27" data-name="Tracciato 27" d="M10.5,10.5h10v10" transform="translate(-10.5 -10.5)" fill="none" stroke="#000" stroke-linejoin="bevel" stroke-width="1.2" />
-                    </g>
-                </svg>
-            </span>
-            <span class="text-2">Prenota una visita
-                <svg xmlns="http://www.w3.org/2000/svg" width="11.024" height="11.024" viewBox="0 0 11.024 11.024" class="ml-1">
-                    <g id="Icon_feather-arrow-up-right" data-name="Icon feather-arrow-up-right" transform="translate(-10.076 -9.9)">
-                        <path id="Tracciato_26" data-name="Tracciato 26" d="M10.5,20.5l10-10" fill="none" stroke="#fff" stroke-linejoin="round" stroke-width="1.2" />
-                        <path id="Tracciato_27" data-name="Tracciato 27" d="M10.5,10.5h10v10" fill="none" stroke="#fff" stroke-linejoin="bevel" stroke-width="1.2" />
-                    </g>
-                </svg>
-            </span>
-            <span class="shape"></span>
-        </a>
+        <p class="ped__content__title"><?php echo esc_html(get_field('titolo_prima_e_dopo', 'option')); ?></p>
+        <div class="ped__content__text wysiwyg"><?php the_field('testo_prima_e_dopo', 'option'); ?></div>
+        <?php
+        $link_ped = get_field('pulsante_prima_e_dopo', 'option');
+        if ($link_ped) :
+            $link_ped_url = $link_ped['url'];
+            $link_ped_title = $link_ped['title'];
+            $link_ped_target = $link_ped['target'] ? $link_ped['target'] : '_self';
+        ?>
+            <a href="<?php echo esc_url($link_ped_url); ?>" target="<?php echo esc_attr($link_ped_target); ?>" class="button-gray mt-3">
+                <span class="text"><?php echo esc_html($link_ped_title); ?>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="11.024" height="11.024" viewBox="0 0 11.024 11.024" class="ml-1">
+                        <g id="Icon_feather-arrow-up-right" data-name="Icon feather-arrow-up-right" transform="translate(0.424 0.6)">
+                            <path id="Tracciato_26" data-name="Tracciato 26" d="M10.5,20.5l10-10" transform="translate(-10.5 -10.5)" fill="none" stroke="#000" stroke-linejoin="round" stroke-width="1.2" />
+                            <path id="Tracciato_27" data-name="Tracciato 27" d="M10.5,10.5h10v10" transform="translate(-10.5 -10.5)" fill="none" stroke="#000" stroke-linejoin="bevel" stroke-width="1.2" />
+                        </g>
+                    </svg>
+                </span>
+                <span class="text-1"><?php echo esc_html($link_ped_title); ?>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="11.024" height="11.024" viewBox="0 0 11.024 11.024" class="ml-1">
+                        <g id="Icon_feather-arrow-up-right" data-name="Icon feather-arrow-up-right" transform="translate(0.424 0.6)">
+                            <path id="Tracciato_26" data-name="Tracciato 26" d="M10.5,20.5l10-10" transform="translate(-10.5 -10.5)" fill="none" stroke="#000" stroke-linejoin="round" stroke-width="1.2" />
+                            <path id="Tracciato_27" data-name="Tracciato 27" d="M10.5,10.5h10v10" transform="translate(-10.5 -10.5)" fill="none" stroke="#000" stroke-linejoin="bevel" stroke-width="1.2" />
+                        </g>
+                    </svg>
+                </span>
+                <span class="text-2"><?php echo esc_html($link_ped_title); ?>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="11.024" height="11.024" viewBox="0 0 11.024 11.024" class="ml-1">
+                        <g id="Icon_feather-arrow-up-right" data-name="Icon feather-arrow-up-right" transform="translate(-10.076 -9.9)">
+                            <path id="Tracciato_26" data-name="Tracciato 26" d="M10.5,20.5l10-10" fill="none" stroke="#fff" stroke-linejoin="round" stroke-width="1.2" />
+                            <path id="Tracciato_27" data-name="Tracciato 27" d="M10.5,10.5h10v10" fill="none" stroke="#fff" stroke-linejoin="bevel" stroke-width="1.2" />
+                        </g>
+                    </svg>
+                </span>
+                <span class="shape"></span>
+            </a>
+        <?php endif; ?>
+
     </div>
     <div class="ped__slider swiperPrima">
         <ul class="swiper-wrapper">
