ed2e592267f9514a51ecb45b9016e2d1879767b3
Federico <fede.toldo96@gmail.com>
2024-04-24 09:59:52 +0200

    404 page

--- 404.php
+++ 404.php
@@ -1,3 +1,33 @@
-<?php get_header();?>
-<div class="placeholder"></div>
-<?php get_footer();?>
+<?php get_header(); ?>
+<main class="error">
+    <h1 class="error__title">Pagina non trovata</h1>
+    <p class="error__text">Errore 404</p>
+    <a href="<?php echo home_url(); ?>" class="button-green">
+        <span class="text">Torna alla Home
+            <svg xmlns="http://www.w3.org/2000/svg" width="11.024" height="11.024" viewBox="0 0 11.024 11.024" class="ml-1">
+                <g id="Icon_feather-arrow-up-right" data-name="Icon feather-arrow-up-right" transform="translate(-10.076 -9.9)">
+                    <path id="Tracciato_26" data-name="Tracciato 26" d="M10.5,20.5l10-10" fill="none" stroke="#fff" stroke-linejoin="round" stroke-width="1.2" />
+                    <path id="Tracciato_27" data-name="Tracciato 27" d="M10.5,10.5h10v10" fill="none" stroke="#fff" stroke-linejoin="bevel" stroke-width="1.2" />
+                </g>
+            </svg>
+        </span>
+        <span class="text-1">Torna alla Home
+            <svg xmlns="http://www.w3.org/2000/svg" width="11.024" height="11.024" viewBox="0 0 11.024 11.024" class="ml-1">
+                <g id="Icon_feather-arrow-up-right" data-name="Icon feather-arrow-up-right" transform="translate(-10.076 -9.9)">
+                    <path id="Tracciato_26" data-name="Tracciato 26" d="M10.5,20.5l10-10" fill="none" stroke="#fff" stroke-linejoin="round" stroke-width="1.2" />
+                    <path id="Tracciato_27" data-name="Tracciato 27" d="M10.5,10.5h10v10" fill="none" stroke="#fff" stroke-linejoin="bevel" stroke-width="1.2" />
+                </g>
+            </svg>
+        </span>
+        <span class="text-2">Torna alla Home
+            <svg xmlns="http://www.w3.org/2000/svg" width="11.024" height="11.024" viewBox="0 0 11.024 11.024" class="ml-1">
+                <g id="Icon_feather-arrow-up-right" data-name="Icon feather-arrow-up-right" transform="translate(-10.076 -9.9)">
+                    <path id="Tracciato_26" data-name="Tracciato 26" d="M10.5,20.5l10-10" fill="none" stroke="#fff" stroke-linejoin="round" stroke-width="1.2" />
+                    <path id="Tracciato_27" data-name="Tracciato 27" d="M10.5,10.5h10v10" fill="none" stroke="#fff" stroke-linejoin="bevel" stroke-width="1.2" />
+                </g>
+            </svg>
+        </span>
+        <span class="shape"></span>
+    </a>
+</main>
+<?php get_footer(); ?>
