d9669604d78f9c45a9c51c7c03ecd841a4e5509e
Federico <fede.toldo96@gmail.com>
2024-04-26 14:45:56 +0200

    table of content single

--- single.php
+++ single.php
@@ -66,7 +66,7 @@
 
                     <!-- Blocco H2 -->
 
-                    <h2 class="articolo__content__h2"><?php echo esc_html($h2) ?></h2>
+                    <h2 id="<?php echo str_replace(' ', '_', esc_html($h2)); ?>" class="articolo__content__h2"><?php echo esc_html($h2); ?></h2>
 
                     <!-- Blocco H3 -->
 
@@ -74,7 +74,7 @@
                 elseif (get_row_layout() == 'titolo_h3') :
                     $h3 = get_sub_field('titolo_h3'); ?>
 
-                    <h3 class="articolo__content__h3"><?php echo esc_html($h3) ?></h3>
+                    <h3 id="<?php echo str_replace(' ', '_', esc_html($h3)); ?>" class="articolo__content__h3"><?php echo esc_html($h3); ?></h3>
 
                     <!-- Blocco H4 -->
 
@@ -82,7 +82,7 @@
                 elseif (get_row_layout() == 'titolo_h4') :
                     $h4 = get_sub_field('titolo_h4'); ?>
 
-                    <h4 class="articolo__content__h4"><?php echo esc_html($h4) ?></h4>
+                    <h4 id="<?php echo str_replace(' ', '_', esc_html($h4)); ?>" class="articolo__content__h4"><?php echo esc_html($h4); ?></h4>
 
                     <!-- Blocco Paragrafo -->
 
@@ -240,7 +240,81 @@
             <span class="articolo__control__buttons__next"><?php next_post_link('%link') ?></span>
         </div>
     </section>
-    <aside class="articolo__sideboard"></aside>
+    <aside class="articolo__sideboard">
+        <ol>
+            <?php
+            if (have_rows('contenuto_articolo')) :
+                // Loop through rows.
+                while (have_rows('contenuto_articolo')) : the_row();
+
+                    if (get_row_layout() == 'titolo_h2') :
+                        $h2 = get_sub_field('titolo_h2'); ?>
+
+                        <!-- Blocco H2 -->
+                        <li>
+                            <a href="#<?php echo str_replace(' ', '_', esc_html($h2)); ?>" class=""><?php echo esc_html(substr($h2, 0, 20)) . '...'; ?></a>
+                        </li>
+                    <?php
+                    elseif (get_row_layout() == 'titolo_h3') :
+                        $h3 = get_sub_field('titolo_h3'); ?>
+                        <!-- Blocco H3 -->
+                        <li>
+                            <a href="#<?php echo str_replace(' ', '_', esc_html($h3)); ?>" class=""><?php echo esc_html(substr($h3, 0, 20)) . '...'; ?></a>
+                        </li>
+
+                    <?php
+                    elseif (get_row_layout() == 'titolo_h4') :
+                        $h4 = get_sub_field('titolo_h4'); ?>
+                        <!-- Blocco H4 -->
+                        <li>
+                            <a href="#<?php echo str_replace(' ', '_', esc_html($h4)); ?>" class=""><?php echo esc_html(substr($h4, 0, 20)) . '...'; ?></a>
+                        </li>
+            <?php
+                    endif;
+                endwhile;
+            endif;
+            ?>
+        </ol>
+        <div class="articolo__sideboard__cta">
+            <h2 class="articolo__sideboard__cta__title"><?php echo esc_html(get_field('titolo_cta_footer', 'option'));?></h2>
+            <?php
+            $cta_link = get_field('bottone_cta_footer', 'option');
+            if ($cta_link) :
+                $cta_link_url = $cta_link['url'];
+                $cta_link_title = $cta_link['title'];
+                $cta_link_target = $cta_link['target'] ? $cta_link['target'] : '_self';
+            ?>
+                <a href="<?php echo esc_url($cta_link_url); ?>" class="articolo__sideboard__cta__button button-white" target="<?php echo esc_attr($cta_link_target); ?>">
+                    <span class="text"><?php echo esc_html($cta_link_title); ?>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="11.024" height="11.024" viewBox="0 0 11.024 11.024" class="ml-1">
+                            <g id="Icon_feather-arrow-up-right" data-name="Icon feather-arrow-up-right" transform="translate(0.424 0.6)">
+                                <path id="Tracciato_26" data-name="Tracciato 26" d="M10.5,20.5l10-10" transform="translate(-10.5 -10.5)" fill="none" stroke="#000" stroke-linejoin="round" stroke-width="1.2" />
+                                <path id="Tracciato_27" data-name="Tracciato 27" d="M10.5,10.5h10v10" transform="translate(-10.5 -10.5)" fill="none" stroke="#000" stroke-linejoin="bevel" stroke-width="1.2" />
+                            </g>
+                        </svg>
+                    </span>
+                    <span class="text-1"><?php echo esc_html($cta_link_title); ?>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="11.024" height="11.024" viewBox="0 0 11.024 11.024" class="ml-1">
+                            <g id="Icon_feather-arrow-up-right" data-name="Icon feather-arrow-up-right" transform="translate(0.424 0.6)">
+                                <path id="Tracciato_26" data-name="Tracciato 26" d="M10.5,20.5l10-10" transform="translate(-10.5 -10.5)" fill="none" stroke="#000" stroke-linejoin="round" stroke-width="1.2" />
+                                <path id="Tracciato_27" data-name="Tracciato 27" d="M10.5,10.5h10v10" transform="translate(-10.5 -10.5)" fill="none" stroke="#000" stroke-linejoin="bevel" stroke-width="1.2" />
+                            </g>
+                        </svg>
+                    </span>
+                    <span class="text-2"><?php echo esc_html($cta_link_title); ?>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="11.024" height="11.024" viewBox="0 0 11.024 11.024" class="ml-1">
+                            <g id="Icon_feather-arrow-up-right" data-name="Icon feather-arrow-up-right" transform="translate(-10.076 -9.9)">
+                                <path id="Tracciato_26" data-name="Tracciato 26" d="M10.5,20.5l10-10" fill="none" stroke="#fff" stroke-linejoin="round" stroke-width="1.2" />
+                                <path id="Tracciato_27" data-name="Tracciato 27" d="M10.5,10.5h10v10" fill="none" stroke="#fff" stroke-linejoin="bevel" stroke-width="1.2" />
+                            </g>
+                        </svg>
+                    </span>
+                    <span class="shape"></span>
+                </a>
+            <?php endif; ?>
+            <img class="articolo__sideboard__cta__image" src="<?php echo get_template_directory_uri() . '/assets/img/cta/cta_mobile_hover.png'; ?>" alt="">
+        </div>
+    </aside>
 </main>
 
 <?php get_footer(); ?>
